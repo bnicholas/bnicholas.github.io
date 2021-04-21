090ebd80256b40cc7f7fb0e3c0157a5c8d015f90
Brian Nicholas <uibri@me.com>
2021-04-21 13:31:05 -0700

    pushit

--- nuleds/keypad/index.js
+++ nuleds/keypad/index.js
@@ -1,28 +1,11 @@
-// register service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/nuleds/sw.js', {
-    scope: '/nuleds/'
-  }).then(function (reg) {
-    if (reg.installing) {
-      console.log('Service worker installing')
-    } else if (reg.waiting) {
-      console.log('Service worker installed')
-    } else if (reg.active) {
-      console.log('Service worker active');
-    }
-  }).catch(function (error) {
-    console.log('Registration failed with ' + error);
-  });
-}
+navigator.serviceWorker.register('/nuleds/sw.js', { scope: '/nuleds/' })
 
+const nuBlu = { server: null, device: null, on: false }
 const encoder = new TextEncoder()
 const serviceUID = "49535343-fe7d-4ae5-8fa9-9fafd205e455"
 const characteristicUID = "49535343-1e4d-4bd9-ba61-23c647249616"
-const onHard = "(NC_Z0A255R255G255B255W255F0)"
-const _onHard = encoder.encode(onHard)
-const offHard = "(NC_Z0A255R0G0B0W0F0)"
-const _offHard = encoder.encode(offHard)
-const nuBlu = { server: null, device: null }
+const onHard = encoder.encode("(NC_Z0A255R255G255B255W255F0)")
+const offHard = encoder.encode("(NC_Z0A255R0G0B0W0F0)")
 
 const handleScan = async e => {
   if (!navigator.bluetooth) alert('NO WEB BLE')
@@ -37,14 +20,16 @@ const handleScan = async e => {
     nuBlu.char = await nuBlu.service.getCharacteristic(characteristicUID)
     nuBlu.onHard = () => nuBlu.char.writeValue(_onHard)
     nuBlu.offHard = () => nuBlu.char.writeValue(_offHard)
-
     // ADD EVENTS TO ALL THE SCENE BUTTONS
     const buttons = Array.from(document.getElementsByClassName('btn'))
     buttons.forEach(b => {
+      b.addEventListener('dblclick', async e => {
+        if (nuBlu.on) nuBlu.char.writeValue(offHard)
+        else nuBlu.char.writeValue(onHard)
+        nuBlu.on = !nuBlu.on
+      })
       b.addEventListener('click', async e => {
-        const value = encoder.encode(e.target.value)
-        nuBlu.char.writeValue(value)
-        console.log(`BUTTON CLICK ${value}`)
+        nuBlu.char.writeValue(encoder.encode(e.target.value))
       })
     })
   }
