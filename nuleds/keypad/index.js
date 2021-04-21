ff435c960eaa9836c6fea99d53353e560878be92
Brian Nicholas <uibri@me.com>
2021-04-21 16:20:08 -0700

    pushit

--- nuleds/keypad/index.js
+++ nuleds/keypad/index.js
@@ -1,35 +1,53 @@
-navigator.serviceWorker.register('/nuleds/sw.js', { scope: '/nuleds/' })
+// navigator.serviceWorker.register('/nuleds/sw.js', {
+//   scope: '/nuleds/'
+// })
 
-const nuBlu = { server: null, device: null, on: false }
+let debounce
 const encoder = new TextEncoder()
 const serviceUID = "49535343-fe7d-4ae5-8fa9-9fafd205e455"
 const characteristicUID = "49535343-1e4d-4bd9-ba61-23c647249616"
-const onHard = encoder.encode("(NC_Z0A255R255G255B255W255F0)")
-const offHard = encoder.encode("(NC_Z0A255R0G0B0W0F0)")
+const onHard = "(NC_Z0A255R255G255B255W255F0)"
+const offHard = "(NC_Z0A255R0G0B0W0F0)"
+
+function sendCommand() {
+  debounce = setTimeout(() => {
+    nu.char.writeValue(encoder.encode(nu.command))
+  }, 100)
+}
+
+const nu = new Proxy({on: true}, {
+  set: function (obj, prop, value) {
+    obj[prop] = value
+    if (prop === "command") {
+      clearTimeout(debounce)
+      sendCommand()
+    }
+    return true
+  }
+})
 
 const handleScan = async e => {
   if (!navigator.bluetooth) alert('NO WEB BLE')
-  nuBlu.device = await navigator.bluetooth.requestDevice({
+  nu.device = await navigator.bluetooth.requestDevice({
     optionalServices: [serviceUID],
     acceptAllDevices: true
   })
-  nuBlu.server = await nuBlu.device.gatt.connect()
-  if (nuBlu.server.connected) {
-    alert(`${nuBlu.device.name} Connected`)
-    nuBlu.service = await nuBlu.server.getPrimaryService(serviceUID)
-    nuBlu.char = await nuBlu.service.getCharacteristic(characteristicUID)
-    nuBlu.onHard = () => nuBlu.char.writeValue(_onHard)
-    nuBlu.offHard = () => nuBlu.char.writeValue(_offHard)
-    // ADD EVENTS TO ALL THE SCENE BUTTONS
+  nu.server = await nu.device.gatt.connect()
+  if (nu.server.connected) {
+    console.log(`${nu.device.name} Connected`)
+    nu.service = await nu.server.getPrimaryService(serviceUID)
+    nu.char = await nu.service.getCharacteristic(characteristicUID)
     const buttons = Array.from(document.getElementsByClassName('btn'))
     buttons.forEach(b => {
-      b.addEventListener('dblclick', async e => {
-        if (nuBlu.on) nuBlu.char.writeValue(offHard)
-        else nuBlu.char.writeValue(onHard)
-        nuBlu.on = !nuBlu.on
+      b.addEventListener('dblclick', () => {
+        nu.command = nu.on ? offHard : onHard
+        nu.on = !nu.on
+        console.log(`CLICK ${nu.command}`)
       })
-      b.addEventListener('click', async e => {
-        nuBlu.char.writeValue(encoder.encode(e.target.value))
+      b.addEventListener('click', e => {
+        nu.on = e.target.className.includes('offBtn') ? false : true
+        nu.command = e.target.value
+        console.log(`CLICK ${nu.command}`)
       })
     })
   }
